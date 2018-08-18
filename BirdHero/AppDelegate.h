c87192840ed4b514e1efd4ef67f20e07b36243cd
nguyen dang <nguyendang@nguyens-macbook-pro.local>
2018-08-18 15:24:34 +0700

    Initial Commit

---
+++ BirdHero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BirdHero
+//
+//  Created by nguyen dang on 8/18/18.
+//  Copyright © 2018 Agritele Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BirdHero/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BirdHero
+//
+//  Created by nguyen dang on 8/18/18.
+//  Copyright © 2018 Agritele Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
